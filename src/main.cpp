abe96165570fa4c333e81579e44daa8d41cb3b1b
eternity <45088542+EternalRift@users.noreply.github.com>
2021-11-05 10:03:48 -0700

    clean up code etc
    make program run as admin by default
    clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,79 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
-std::vector<std::string> blocked_sites;
+using vector_t = std::vector<std::string>;
 
-void fetchDomains();
-void read(std::vector<std::string> *lines);
-void write(std::vector<std::string> vec, std::ofstream &hosts_file);
+namespace vars
+{
+  vector_t blocked_sites{};
+}
 
-int main() {
+vector_t read()
+{
+  vector_t lines{};
 
-    fetchDomains();
+  std::ifstream hosts("C:\\Windows\\System32\\drivers\\etc\\hosts");
+  std::string line{};
 
-    std::ofstream hosts_file;
-    hosts_file.open(R"(C:\Windows\System32\drivers\etc\hosts)", std::ios::app);
-
-    if (!hosts_file) {
-        std::cout << "Unable to access hosts file! Try running as Administrator." << std::endl;
-        getchar();
-        return -1; //no access
+  while (std::getline(hosts, line))
+  {
+    if (line.find("127.0.0.1") != std::string::npos)
+    {
+      lines.push_back(line);
     }
+  }
+
+  return lines;
+}
+
+bool write(vector_t& vec)
+{
+  int blacklisted{ 0 };
+
+  std::ofstream hosts_file{ L"C:\\Windows\\System32\\drivers\\etc\hosts", std::ios::app };
+
+  // this won't ever happen
+  if (!hosts_file)
+    std::exit(-1); 
+
+  for (const auto& site : vars::blocked_sites)
+  {
+    if (std::find(vec.begin(), vec.end(), "127.0.0.1     " + site) == vec.end()) 
+    {
+      hosts_file << "\n127.0.0.1     " << site;
+      std::cout << "Blacklisted " << site << '\n';
+      ++blacklisted;
+    }
+  }
+
+  std::cout << "Finished blacklisting " 
+    << blacklisted 
+    << " sites! Press any key to close\n";
+}
+
+void fetch_domains() 
+{
+  system("C:\\Windows\\System32\\curl \
+    https://raw.githubusercontent.com/GardeningTool/HostsMod/main/domains.txt > domains.txt");
+
+  std::ifstream domains("domains.txt");
+  std::string line{};  
+
+  while (std::getline(domains, line)) 
+  {
+    vars::blocked_sites.push_back(line);
+  }
+}
+
+int main()
+{
+  fetch_domains();
     
-    std::vector<std::string> current;
-    read(&current);
-    write(current, hosts_file);
-    hosts_file.close();
+  vector_t current{ read() };
 
-    getchar();
-}
+  write(current);
 
-void read(std::vector<std::string> *lines) {
-    std::string line;
-    std::ifstream in (R"(C:\Windows\System32\drivers\etc\hosts)");
-    while (getline(in,line)) {
-        if (line.find("127.0.0.1") != std::string::npos) {
-            lines->push_back(line);
-        }
-    }
-    in.close();
-}
-
-void write(std::vector<std::string> vec, std::ofstream &hosts_file) {
-    int blacklisted = 0;
-    for (const auto &site : blocked_sites) {
-        if (find(vec.begin(), vec.end(), "127.0.0.1     " + site) == vec.end()) {
-            hosts_file << "\n127.0.0.1     " << site;
-            std::cout << "Blacklisted " << site << std::endl;
-            ++blacklisted;
-        }
-    }
-    std::cout << "Finished blacklisting " << blacklisted << " sites! Press any key to close." << std::endl;
-}
-
-void fetchDomains() {
-    system(R"(C:\Windows\System32\curl https://raw.githubusercontent.com/GardeningTool/HostsMod/main/domains.txt > domains.txt)");
-    std::string line;
-    std::ifstream in ("domains.txt");
-    while (getline(in, line)) {
-        blocked_sites.push_back(line);
-    }
+  return std::cin.get();
 }
